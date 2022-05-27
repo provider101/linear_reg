eb73aa4152d5a496dfcf951d3a05655f984cf226
provider101 <akkyw143@gmail.com>
2022-05-27 14:06:36 +0530

    api2

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from re import L
 from flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
 def grad_des():
     return 'api1'
+
+def grad_desc1():
+    return 'api2'
 
 if __name__=="__main__":
     app.run(debug=True)
